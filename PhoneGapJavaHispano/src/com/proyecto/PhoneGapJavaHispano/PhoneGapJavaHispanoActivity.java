b1dff816af361d0a403f97b8b50bab197d9cb0f7
jortegac <jortegac@ALC-4L82G5J.usersad.everis.int>
2013-10-11 18:42:33 +0200

    PhoneGap

---
+++ PhoneGapJavaHispano/src/com/proyecto/PhoneGapJavaHispano/PhoneGapJavaHispanoActivity.java
@@ -0,0 +1,15 @@
+package com.proyecto.PhoneGapJavaHispano;
+
+import org.apache.cordova.DroidGap;
+
+import android.os.Bundle;
+
+public class PhoneGapJavaHispanoActivity extends DroidGap {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        super.setIntegerProperty("splashscreen", R.drawable.splash);
+        super.loadUrl("file:///android_asset/www/index.html",4000);
+    }
+}
