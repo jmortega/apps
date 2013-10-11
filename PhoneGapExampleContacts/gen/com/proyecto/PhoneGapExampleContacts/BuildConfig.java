adca6e8dc1f87b7286cc91c1fe3856ae30df54f2
jortegac <jortegac@ALC-4L82G5J.usersad.everis.int>
2013-10-11 19:16:24 +0200

    phonegap

---
+++ PhoneGapExampleContacts/gen/com/proyecto/PhoneGapExampleContacts/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.proyecto.PhoneGapExampleContacts;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
