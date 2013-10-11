46cf2b0cb00917cf8884a063a542a90785bad39d
jortegac <jortegac@ALC-4L82G5J.usersad.everis.int>
2013-10-11 17:36:46 +0200

    Flickr Search Jquery Mobile

---
+++ FlickrSearch/src/com/proyecto/FlickrSearch/FlickrSearchActivity.java
@@ -0,0 +1,15 @@
+package com.proyecto.FlickrSearch;
+
+import org.apache.cordova.DroidGap;
+
+import android.os.Bundle;
+
+public class FlickrSearchActivity extends DroidGap {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        super.setIntegerProperty("splashscreen", R.drawable.splash);
+        super.loadUrl("file:///android_asset/www/index.html",3000);
+    }
+}
